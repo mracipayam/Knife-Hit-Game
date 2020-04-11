6c47eab432592be268634bf834edc7f6fc39d75e
Murat Han Acıpayam <acipayammurathan@gmail.com>
2020-04-11 21:21:54 +0300

    Add files via upload

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject restartButton;
+
+    [Header("Knife Count Display")]
+    [SerializeField]
+    private GameObject panelKnives;
+    [SerializeField]
+    private GameObject iconKnife;
+    [SerializeField]
+    private Color usedKnifeIconColor;
+
+
+    public void ShowRestartButton()
+    {
+        restartButton.SetActive(true);
+    }
+
+    public void SetInitialDisplayedKnifeCount(int count)
+    {
+        for(int i = 0; i < count; i++)
+        {
+            Instantiate(iconKnife, panelKnives.transform);
+        }
+    }
+
+    private int knifeIconIndexToChange = 0;
+    public void DecrementDisplayedKnifeCount()
+    {
+        panelKnives.transform.GetChild(knifeIconIndexToChange++)
+            .GetComponent<Image>().color = usedKnifeIconColor;
+    }
+}
